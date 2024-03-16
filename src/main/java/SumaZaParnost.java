774aa51930763aa89e7f6f0382936d836b3abe47
sm791600 <stojanovikmarko7916@gmail.com>
2024-03-16 21:50:46 +0100

    initialisation

---
+++ src/main/java/SumaZaParnost.java
@@ -0,0 +1,12 @@
+/* Корисникот од тастатура внесува 5 броеви, провери дали броевите се парни или
+непарни а потоа испечати ја сумата на парните и сумата на непарните броеви
+соодветно. */
+
+public class SumaZaParnost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
